0eee331cba935346beee80cd11e80d3070f6ce0c
siwangqishiq <525647740@qq.com>
2015-07-28 10:36:45 +0800

    Initial commit

---
+++ app/src/main/java/com/xinlan/viewdraghelper/CustomView.java
@@ -0,0 +1,114 @@
+package com.xinlan.viewdraghelper;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by panyi  on 2015/7/28.
+ */
+public class CustomView extends LinearLayout {
+    private ViewDragHelper mDragHelper;
+
+    public CustomView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        setOrientation(LinearLayout.VERTICAL);
+        mDragHelper = ViewDragHelper.create(this, 1.0f, new DragCallback());
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (mDragHelper.shouldInterceptTouchEvent(event)) {
+            return true;
+        }
+        return mDragHelper.shouldInterceptTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    private final class DragCallback extends ViewDragHelper.Callback {
+
+        //捕获要拖动的子View
+        @Override
+        public boolean tryCaptureView(View view, int i) {
+            return true;
+        }
+
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            return left;
+        }
+
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            return top;
+        }
+
+        public void onViewDragStateChanged(int state) {
+        }
+
+        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+        }
+
+        public void onViewCaptured(View capturedChild, int activePointerId) {
+        }
+
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+        }
+
+        public void onEdgeTouched(int edgeFlags, int pointerId) {
+        }
+
+        public boolean onEdgeLock(int edgeFlags) {
+            return false;
+        }
+
+        public void onEdgeDragStarted(int edgeFlags, int pointerId) {
+        }
+
+        public int getOrderedChildIndex(int index) {
+            return index;
+        }
+
+        public int getViewHorizontalDragRange(View child) {
+            return 0;
+        }
+
+        public int getViewVerticalDragRange(View child) {
+            return 0;
+        }
+    }//end inner class
+}//end class
